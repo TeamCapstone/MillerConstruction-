d2e316448b3ad144a1ffa99836209502f88c364c
OliverBourland <jeremyredthorn@gmail.com>
2017-04-09 21:33:23 -0700

    Client User Models

---
+++ CapStoneProject/src/CapStoneProject/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapStoneProject.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public int ProjectID { get; set; }
+        public int ClientID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
