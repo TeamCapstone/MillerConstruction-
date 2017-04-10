6fc18f54a287d5837e15075ff673aee5bfd01d0d
Cody Noteboom <knote72@hotmail.com>
2017-04-10 16:02:05 -0700

    project model

---
+++ CapStoneProject/src/CapStoneProject/Models/Project.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CapStoneProject.Models
+{
+    public class Project
+    {
+        public int ProjectID { get; set; }
+
+        //Figure we'll need this to be required but not setting it [Required] yet just in case
+        public int ClientID { get; set; }
+
+        [Required]
+        public int QuoteID { get; set; }
+
+        //Company name (if applicable)
+        public string CompanyName { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal StartFee { get; set; }
+
+        public decimal OtherFees { get; set; }
+
+        [Required]
+        public string ProjectName { get; set; }
+
+        public string ProjectStatus { get; set; }
+
+        //Date when status update was made
+        public DateTime StatusDate { get; set; }
+    }
+}
