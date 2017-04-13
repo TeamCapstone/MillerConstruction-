e09299168e57395b8b9b6e8cfef4ee79d3fa849a
JSR103 <silva.joel.r@gmail.com>
2017-04-13 12:55:28 -0700

    added Review Interface -Joel

--- CapStoneProject/src/CapStoneProject/Repositories/Interfaces/IReviewRepo.cs
+++ CapStoneProject/src/CapStoneProject/Repositories/Interfaces/IReviewRepo.cs
@@ -9,5 +9,16 @@ namespace CapStoneProject.Repositories.Interfaces
 {
     public interface IReviewRepo
     {
+        IQueryable<Review> GetAllMessages();
+
+        List<Review> GetMessageBySubject(string subject);
+
+        List<Review> GetMessageByMember(User user);
+
+        IEnumerable<Review> Messages { get; }
+
+        int Update(Review message);
+
+        Review DeleteMessage(int messageID);
     }
 }
