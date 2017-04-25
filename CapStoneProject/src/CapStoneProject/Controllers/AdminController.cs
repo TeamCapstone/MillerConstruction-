532c08b8637ccedbc1f211febee4ec2cb1ce898e
OliverBourland <jeremyredthorn@gmail.com>
2017-04-25 16:22:32 -0700

    commit

--- CapStoneProject/src/CapStoneProject/Controllers/AdminController.cs
+++ CapStoneProject/src/CapStoneProject/Controllers/AdminController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using CapStoneProject.Models.ViewModels;
 using CapStoneProject.Models;
--- CapStoneProject/src/CapStoneProject/Models/ViewModels/VMCreateUser.cs
+++ CapStoneProject/src/CapStoneProject/Models/ViewModels/VMCreateUser.cs
@@ -11,6 +11,7 @@ namespace CapStoneProject.Models.ViewModels
         [Required]
         public string Email { get; set; }
         [Required]
+        [RegularExpression("^.*(?=.{10,})(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", ErrorMessage = "Must be ten characters long upper and lower case, digit and special character")]
         public string Password { get; set; }
     }
 }
