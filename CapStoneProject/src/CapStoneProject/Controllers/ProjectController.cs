8aef62fdee70d2b28fc441a99ea015d9a7e5c409
Cody Noteboom <knote72@hotmail.com>
2017-04-25 13:48:52 -0700

    projrepoedit

--- CapStoneProject/src/CapStoneProject/Controllers/ProjectController.cs
+++ CapStoneProject/src/CapStoneProject/Controllers/ProjectController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Authorization;
 using CapStoneProject.Models;
 using CapStoneProject.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
+using CapStoneProject.Repositories;
+using CapStoneProject.Repositories.Interfaces;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,15 +16,30 @@ namespace CapStoneProject.Controllers
 {
     public class ProjectController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index() //This one is for admin to view projects
+        private readonly ApplicationDbContext context;
+        private IProjectRepo projectRepo;
+        private UserManager<UserIdentity> userManager;
+        private SignInManager<UserIdentity> signInManager;
+
+        public ProjectController(ApplicationDbContext ctx, IProjectRepo projRepo, 
+            UserManager<UserIdentity> usrMgr, SignInManager<UserIdentity> signInMgr)
         {
-            return View();
+            context = ctx;
+            projectRepo = projRepo;
+            userManager = usrMgr;
+            signInManager = signInMgr;
         }
 
-        public IActionResult ClientProjects(int ClientID) //This one is for a client to view his/her projects
+        [Authorize]
+        public IActionResult Index(string projStatus) //This one is for admin to view currentprojects
         {
-            return View();
+            return View(projectRepo.GetAllCurrentProjects(projStatus));
+        }
+
+        [Authorize]
+        public IActionResult ClientProjects(int clientID) //This one is for a client to view his/her projects
+        {
+            return View("ClientProjects", projectRepo.GetProjectsByClient(clientID));
         }
     }
 }
