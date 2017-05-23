69e209cdc1dbd5e7ecb2505c5b5d46b905518cb9
Cody Noteboom <knote72@hotmail.com>
2017-05-23 15:40:44 -0700

    seed

--- CapStoneProject/src/CapStoneProject/Models/ViewModels/VMCreateProject.cs
+++ CapStoneProject/src/CapStoneProject/Models/ViewModels/VMCreateProject.cs
@@ -9,15 +9,19 @@ namespace CapStoneProject.Models.ViewModels
     public class VMCreateProject
     {
         /*When creating project, decide whether to instantiate client object by entering either
-         client ID, client name, email, etc. Figure out also how to tie in bid object*/
-
+         client ID, client name, email, etc*/
+   
         //temporarily using client last name and email to search for client in db
-        public int ProjectID { get; set; } //To be used by EditProject
 
+        //To be used by EditProject
+        public int ProjectID { get; set; } 
+
+        //to link
         public int BidID { get; set; }
 
         public string LastName { get; set; }
 
+        //to link client to project
         public int ClientID { get; set; }
 
         [Required]
